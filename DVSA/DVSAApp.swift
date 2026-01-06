38d658edb630296bbee38f6a4519ae006c617e02
Rix <alexandru.udrea39@gmail.com>
2026-01-06 16:54:53 +0200

    Initial Commit

---
+++ DVSA/DVSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVSAApp.swift
+//  DVSA
+//
+//  Created by Bastion on 06.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct DVSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
